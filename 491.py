d5e8d2b022e4c823ab23b82532f665d7fc84c65e
deweythedeci <a874589@gmail.com>
2023-02-07 17:07:42 -0800

    finished 491

--- 491.py
+++ 491.py
@@ -1,27 +1,34 @@
 from typing import List
 
-# incomplete
-
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        seqs = self.subseqHelper(nums, [])
-        
-
-    def subseqHelper(self, nums, seq):
+        seqs = []
+        self.subseqHelper(nums, [], seqs)
+        return self.unique(seqs)
+    
+    def subseqHelper(self, nums, seq, seqs):
         if nums == []:
             if len(seq) < 2:
-                return []
+                return
             if not self.isAscending(seq):
-                return []       
-            return [seq]
+                return
+            seqs.append(seq)
         else:
-            return self.subseqHelper(nums[1:], seq + [nums[0]]) + self.subseqHelper(nums[1:], seq)
+            self.subseqHelper(nums[1:], seq + [nums[0]], seqs)
+            self.subseqHelper(nums[1:], seq, seqs)
     
     def isAscending(self, seq):
         for i in range(1,len(seq)):
             if(seq[i-1] > seq[i]):
                 return False
         return True
+
+    def unique(self, arr):
+        new_arr = []
+        for elem in arr:
+            if elem not in new_arr:
+                new_arr.append(elem)
+        return new_arr
 
 s = Solution()
 print("actual",s.findSubsequences([4,3,7,6]))
