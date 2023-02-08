3289f2146a533c17a28075c10c66d70adc5fde99
deweythedeci <a874589@gmail.com>
2023-02-07 16:54:57 -0800

    updated

--- 491.py
+++ 491.py
@@ -1,29 +1,28 @@
 from typing import List
-import copy
 
 # this shit dont work
 
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        return self.findSubseqHelper(nums, [])
+        seqs = self.subseqHelper(nums, [])
         
-    def findSubseqHelper(self, nums, curr):
-        subseqs = []
-        if len(curr):
-            i = 0
-            while(i < len(nums)):
-                if nums[i] < curr[-1]:
-                    nums.pop(i)
-                i += 1
-        if not len(nums):
-            return [curr]
-        for i in range(len(nums)):
-            curr.append(nums.pop(i))
-            subseqs.append(copy.deepcopy(curr))
-            subseqs.extend(self.findSubseqHelper(copy.deepcopy(nums), copy.deepcopy(curr)))
-            nums.insert(i, curr.pop())
-        return subseqs
+
+    def subseqHelper(self, nums, seq):
+        if nums == []:
+            if len(seq) < 2:
+                return []
+            if not self.isAscending(seq):
+                return []       
+            return [seq]
+        else:
+            return self.subseqHelper(nums[1:], seq + [nums[0]]) + self.subseqHelper(nums[1:], seq)
+    
+    def isAscending(self, seq):
+        for i in range(1,len(seq)):
+            if(seq[i-1] > seq[i]):
+                return False
+        return True
 
 s = Solution()
-print("actual",s.findSubsequences([4,6,7,7]))
-print("expected",[[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]])
+print("actual",s.findSubsequences([4,3,7,6]))
+print("expected",[[4,7],[4,6],[3,7],[3,6]])
